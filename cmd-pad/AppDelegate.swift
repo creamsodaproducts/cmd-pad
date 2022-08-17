382f325f47ca8159dcf60e4ccca245a34b1221af
yuzgit <ofnmk.xcode.05@gmail.com>
2022-08-17 20:32:42 +0900

    Initial Commit

---
+++ cmd-pad/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  cmd-pad
+//
+//  Created by atelier on 2022/08/17.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
